c9618574da1d0b56a5284703fd07e36a44b4be85
Mukul <mukulkundu22@gmail.com>
2025-08-15 00:10:43 +0530

    utils code, mainly HOCs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-// import dotenv from "dotenv";
 import 'dotenv/config'
 import connectDB from "./db/index.js";
+import app from './app.js';
 
 
-// dotenv.config({
-//     path: './.env'
-// })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is Running at port: ${process.env.PORT}`);  
+    })
+
+    // app.on("error", (error) => {
+    //     console.log("Express Error : ", error);
+    //     throw error
+    // })
+})
+.catch((err) => {
+    console.log("MONGODB Connection Failed !!! ", err);
+    
+})
